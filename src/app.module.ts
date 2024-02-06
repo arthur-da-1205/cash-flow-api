de9f6b6808c679034e74b2b28d3661f42b40b84e
Arthur Darwanto <75304260+arthur-da-1205@users.noreply.github.com>
2024-02-06 15:00:32 +0700

    Product: create, find all, update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ProductCategoryModule } from './product-category/product-category.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ProductCategoryModule } from './product-category/product-category.modul
     PrismaModule,
     AuthModule,
     ProductCategoryModule,
+    ProductModule,
   ],
 })
 export class AppModule {}
---
+++ src/product/dto/create-product.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  categoryId: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+}
